2f4ae66f64442210aa8f2a0e55b427c6d1eda238
byohannes <byohannes44@gmail.com>
2020-11-16 21:36:25 +0000

    updated product page to include add to basket function

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,29 +1,45 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "../../StateProvider";
 
-const Product = ({ id, title, image, price, rating }) => {
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue(); 
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
         <p>{title}</p>
         <p className="product__price">
-          <small>£</small>
+          <small>$</small>
           <strong>{price}</strong>
         </p>
         <div className="product__rating">
           {Array(rating)
             .fill()
             .map((_, i) => (
-              <p>🌟</p>
+              <p key={i}>🌟</p>
             ))}
         </div>
       </div>
 
-      <img src={image} alt="" />
+      <img src={image} alt="ProductImage" />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
-};
+}
 
 export default Product;
