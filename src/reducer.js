0e1d327addc639298f30caa10a51a4e57ca963a8
byohannes <byohannes44@gmail.com>
2020-11-18 03:18:52 +0000

    updated reducer actions with comment

--- src/reducer.js
+++ src/reducer.js
@@ -7,6 +7,8 @@ export const initialState = {
 export const getBasketTotal = (basket) =>
   basket?.reduce((amount, item) => item.price + amount, 0);
 
+// Reducer actions
+
 const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
